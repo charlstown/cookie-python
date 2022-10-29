cec0fb114526c8ba812579f43a6350f59c05d58d
charlstown <carlosgranden@gmail.com>
2022-10-29 16:42:57 +0200

    [update] Cookiecutter json file & template working & ready

---
+++ {{cookiecutter.project_name}}/README.md
@@ -0,0 +1,43 @@
+# {cookiecutter.project_name}
+{cookiecutter.one_line_description}
+
+*Repository url:* https://github.com/{cookiecutter.user_git}/{cookiecutter.project_name}
+
+---
+
+## 1. Description
+
+Sum up your application in a few lines, 30 - 60 words. Use **bold font** to highlight your keywords and *italic* for links or references.
+
+## 2. Installation
+
+How to set up all requirements, versions everything needed to start running the app.
+
+#### 	2.1 Sections
+
+​	Create sub-sections if needed to divide the Installation by a hierarchy.
+
+## 3. Usage
+
+Explain all the steps to execute your app. Remember if it's for Linux/mac or windows or both, and help yourself with coding snippets to explain the commands.
+
+```
+# Example of a code snippet
+python code/myapp.py -MyFlags
+```
+
+## 4. Troubleshooting
+
+Errors, future updates, or beta configurations. Explain why and how it could break.
+
+## 5. Disclaimer
+
+Explain the do's and don'ts of your app, what you take responsibility for, and where is the limit of its purpose.
+
+## 6. Help wanted
+
+Some extra help or clarification about the whole process. What you don't provide.
+
+## 7. Other links
+
+References, contact or related repositories go here.
---
+++ {{cookiecutter.project_name}}/code/main.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# -----------------------------------------------------
+# Configuration file for the project {{cookiecutter.project_name}}
+# Author/s: {{cookiecutter.author}}
+# -----------------------------------------------------
+# Date: {% now 'local', '%d/%m/%Y' %}
+# License: {{cookiecutter.license}}
+# Version: {{cookiecutter.version}}
+# Maintainer: {{cookiecutter.maintainer}}
+# Contact: {{cookiecutter.contact}}
+# -----------------------------------------------------
+
+# Libraries
+import argparse
+import yaml
+import logging
+
+
+class App:
+    """
+    Class description: Explain the main class.
+    Name the public methods from this class:
+    """
+
+    def __init__(self, logger: logging, dir_config: str):
+        """
+        Class constructor: Here you should read the config file, generate
+        the instances from modules and declare global variables.
+        :param logger:
+        :param dir_config: Explain the dir_config parameter.
+        """
+        # Reading the config json file
+        yaml_file = open(dir_config, 'r')
+        config = yaml.safe_load(yaml_file)
+
+        # Variables
+        self.config = config
+        self.log = logger
+
+        # Instance
+
+    def run(self):
+        """
+        This method runs the whole app and manage all calls.
+        :return:
+        """
+        self.log.info("Initializing the app")
+        # Run first module method
+        self.log.info(self.config)
+
+        # Run second module method
+
+
+# Starting the app when main
+if __name__ == "__main__":
+    # Initialize the argument parser
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--config", "-c", default="data/config.yaml",
+                        help="Add the config file path after this flag")
+    parser.add_argument("--test", "-t", default=False, action='store_true',
+                        help="This argument is a switcher, by default is false")
+    args = parser.parse_args()
+
+    # Argument variables
+    arg_config = args.config
+    arg_test = args.test
+    logging.basicConfig(level=logging.INFO)
+    my_logger = logging.getLogger('APP')
+    print("Initial args:")
+    print(f"- Config: {arg_config}")
+    print(f"- Test: {arg_test}", '\n')
+
+    # App execution
+    my_app = App(logger=my_logger, dir_config=arg_config)
+    my_app.run()
