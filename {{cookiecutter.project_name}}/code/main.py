d965b9ba521085eae9438e7074a0d249a7c22a9e
charlstown <carlosgranden@gmail.com>
2022-11-01 13:30:40 +0100

    [Update] Logger restructured

--- {{cookiecutter.project_name}}/code/main.py
+++ {{cookiecutter.project_name}}/code/main.py
@@ -26,13 +26,28 @@ class App:
     Name the public methods from this class:
     """
 
-    def __init__(self, logger: logging, dir_config: str):
+    def __init__(self, args: argparse.Namespace):
         """
         Class constructor: Here you should read the config file, generate
         the instances from modules and declare global variables.
         :param logger:
         :param dir_config: Explain the dir_config parameter.
         """
+        # Argument variables
+        dir_config = args.config
+        test = args.test
+        arg_level = args.log[0]
+
+        # Getting logger
+        logger = self._get_logger(level=arg_level)
+
+        # Logging argument variables
+        logger.debug('')
+        logger.debug("Initial args: ")
+        for k, v in vars(args).items():
+            logger.debug(f">> {k}: {v}")
+        logger.debug("\n")
+
         # Reading the config json file
         yaml_file = open(dir_config, 'r')
         config = yaml.safe_load(yaml_file)
@@ -43,6 +58,31 @@ class App:
 
         # Global instances
         self.test = Test(logger=logger, config=config)
+
+    def _get_logger(self, level: str):
+        # Setting up the output level
+        levels = {'debug': logging.DEBUG,
+                  'info': logging.INFO,
+                  'warning': logging.WARNING}
+        set_level = levels[level]
+
+        # Setting up the logger
+        set_log_format = '%(asctime)s [%(levelname)s] %(filename)s (L%(lineno)s) - %(funcName)s: %(message)s'
+        set_date_format = '%Y-%m-%d %H:%M:%S'
+        logging.basicConfig(level=set_level,
+                            format=set_log_format,
+                            datefmt=set_date_format)
+        my_logger = logging.getLogger(__name__)
+
+        # Create a file log handler
+        file_handler = logging.FileHandler('data/logs/file.log')
+        file_handler.setLevel(logging.DEBUG)
+        f_format = logging.Formatter(set_log_format)
+        file_handler.setFormatter(f_format)
+
+        # Add handlers to the logger
+        my_logger.addHandler(file_handler)
+        return my_logger
 
     def run(self):
         """
@@ -63,38 +103,16 @@ if __name__ == "__main__":
     parser.add_argument("--config", "-c",
                         default="data/config.yaml",
                         help="Add the config file path after this flag")
+    parser.add_argument('--log', "-l",
+                        choices=['debug', 'info', 'warning'],
+                        default="INFO",
+                        nargs="+")
     parser.add_argument("--test", "-t",
                         default=False,
                         action='store_true',
                         help="This argument is a switcher, by default is false")
-    args = parser.parse_args()
-
-    # Argument variables
-    arg_config = args.config
-    arg_test = args.test
-
-    # Setting up the logger and output level
-    logging.basicConfig(level=logging.INFO,
-                        format='%(asctime)s [%(levelname)s] %(filename)s (L%(lineno)s) - %(funcName)s: %(message)s',
-                        datefmt='%Y-%m-%d %H:%M:%S',)
-    my_logger = logging.getLogger(__name__)
-
-    # Create file handler
-    file_handler = logging.FileHandler('data/logs/file.log')
-    file_handler.setLevel(logging.DEBUG)
-    f_format = logging.Formatter('%(asctime)s.%(msecs)00d [%(levelname)s] %(filename)s (L%(lineno)s) - %(funcName)s: %(message)s')
-    file_handler.setFormatter(f_format)
-
-    # Add handlers to the logger
-    my_logger.addHandler(file_handler)
-
-    my_logger.info("Initial args:")
-
-    # Logging argument variables
-    for k, v in vars(args).items():
-        my_logger.info(f"- {k}: {v}")
-    my_logger.info("\n")
+    my_args = parser.parse_args()
 
     # App execution
-    my_app = App(logger=my_logger, dir_config=arg_config)
+    my_app = App(args=my_args)
     my_app.run()
