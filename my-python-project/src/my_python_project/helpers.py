6f2189e6bb5814e7f03f8cd114ac541f2508e0d1
Carlos Grande <32444001+charlstown@users.noreply.github.com>
2022-12-12 01:06:59 +0000

    [doc] updated user guide wip

---
+++ my-python-project/src/my_python_project/helpers.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# -----------------------------------------------------
+# Project: my-python-project
+# Author/s: Carlos Grande
+# Maintainer/s: Carlos Grande
+# -----------------------------------------------------
+
+# Libraries
+import logging
+
+
+class Helpers:
+    """
+    Example class as a template for other classes. Explain the use of this class here.
+    """
+    def __init__(self, logger: logging.Logger, config: dict):
+        """
+        Constructor method to create an instance from the class with initial arguments and global variables
+        :param logger: logger defined in the main file
+        :param config: configuration parameters
+        """
+        # Global variables
+        self.logger = logger
+        self.config = config
+        self.example_global_variable = "Hello World"
+
+    def _private_method(self):
+        """
+        Explain here the usage of this method
+        :return: None
+        """
+        self.logger.info(f'This "{self._private_method.__name__}" is a method from {Helpers.__name__}.')
+
+    def public_method(self):
+        """
+        Explain here the usage of this method
+        :return: None
+        """
+        self.logger.info(f'This "{self.public_method.__name__}" is a method from {Helpers.__name__}.')
+        self._private_method()
+        self.logger.info(f'This "{self.example_global_variable}" is a global variable.')
